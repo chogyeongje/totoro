d80bc49c3d4f039a2a85c4024b53ee236a207870
chogyeongje <avq159@gmail.com>
2021-05-11 00:58:31 +0900

    change generate_tfrecord.py

--- tfrecord/generate_tfrecord.py
+++ tfrecord/generate_tfrecord.py
@@ -29,12 +29,16 @@ FLAGS = flags.FLAGS
 
 # TO-DO replace this with label map
 def class_text_to_int(row_label):
-    if row_label == 'totoro':
-    # if row_label == 'tommad':
+    # if row_label == 'totoro':
+    # # if row_label == 'tommad':
+    #     return 1
+    # elif row_label == 'nekobus':
+    # # elif row_label == 'kk':
+    #     return 2
+    # else:
+    #     None
+    if row_label == 'tree':
         return 1
-    elif row_label == 'nekobus':
-    # elif row_label == 'kk':
-        return 2
     else:
         None
 
